2182f47d93fb671f8b6a87e3696580133e1c6609
bawantha395 <Bawantharathnayake395@gmail.com>
2024-03-22 23:47:25 +0530

    input

--- java_basics/src/HelloWorld.java
+++ java_basics/src/HelloWorld.java
@@ -1,10 +1,19 @@
+import java.util.Scanner;
+
+
 public class HelloWorld {
     public static void main(String[] args) {
-        double GPA = 3.02;
+        double sGPA = 3.02;
         String firstName = "abc";
         String lastName = "xyz";
 
-        System.out.println(firstName + " " + lastName + " " + "has a GPA " + GPA);
+        System.out.println(firstName + " " + lastName + " " + "has a GPA " + sGPA);
+
+        System.out.println("enter new Gpa");
+
+        Scanner input = new Scanner(System.in);
+        sGPA = input.nextDouble();
+        System.out.println(firstName + " " + lastName + " " + " now has a GPA " + sGPA);
     }
 
 }
