db7a98d986f255471d204b854b9e5fac3bc2760d
bawantha395 <Bawantharathnayake395@gmail.com>
2024-03-23 12:09:03 +0530

    if-else

--- java_basics/src/HelloWorld.java
+++ java_basics/src/HelloWorld.java
@@ -3,17 +3,32 @@ import java.util.Scanner;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        double sGPA = 3.02;
-        String firstName = "abc";
-        String lastName = "xyz";
+//        double sGPA = 3.02;
+//        String firstName = "abc";
+//        String lastName = "xyz";
+//
+//        System.out.println(firstName + " " + lastName + " " + "has a GPA " + sGPA);
+//
+//        System.out.println("enter new Gpa");
+//
+//        Scanner input = new Scanner(System.in);
+//        sGPA = input.nextDouble();
+//        System.out.println(firstName + " " + lastName + " " + " now has a GPA " + sGPA);
 
-        System.out.println(firstName + " " + lastName + " " + "has a GPA " + sGPA);
 
-        System.out.println("enter new Gpa");
-
-        Scanner input = new Scanner(System.in);
-        sGPA = input.nextDouble();
-        System.out.println(firstName + " " + lastName + " " + " now has a GPA " + sGPA);
+        int number;
+        System.out.println("enter the number : ");
+        Scanner input =new Scanner(System.in);
+        number = input.nextInt();
+        if(number>8){
+            System.out.println("high");
+        }
+        else if (number >5){
+            System.out.println("medium");
+        }
+        else{
+            System.out.println("low");
+        }
     }
 
 }
