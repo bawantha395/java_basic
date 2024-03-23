4fc5305893cf3fa0f1860b4b3c25b9545188c358
bawantha395 <Bawantharathnayake395@gmail.com>
2024-03-23 19:34:14 +0530

    functions

--- java_basics/src/HelloWorld.java
+++ java_basics/src/HelloWorld.java
@@ -2,6 +2,14 @@ import java.util.Scanner;
 
 
 public class HelloWorld {
+
+    public static void announceTeatime(){
+        System.out.println("wait for a tea time");
+        System.out.println("type word" + "  to start tea time");
+        Scanner input = new Scanner(System.in);
+         input.next();
+        System.out.println("this is a teatime");
+    }
     public static void main(String[] args) {
 //        double sGPA = 3.02;
 //        String firstName = "abc";
@@ -30,20 +38,54 @@ public class HelloWorld {
 //            System.out.println("low");
 //        }
 
-        Scanner input = new Scanner(System.in);
+//        Scanner input = new Scanner(System.in);
 
-        boolean isSongRepeat =  true;
+//        boolean isSongRepeat =  true;
+//
+//        while(isSongRepeat){
+//            System.out.println("current song is playing");
+//            System.out.println("are you want to repaet");
+//            String song = input.next();
+//
+//            if("yes".equals(song)){
+//                isSongRepeat = false;
+//            }
+//        }
+//        System.out.println("playing next song");
 
-        while(isSongRepeat){
-            System.out.println("current song is playing");
-            System.out.println("are you want to repaet");
-            String song = input.next();
-
-            if("yes".equals(song)){
-                isSongRepeat = false;
-            }
-        }
-        System.out.println("playing next song");
+//        String question = "What is the largest planet in our solar system";
+//        String choiceOne = "earth";
+//        String choiceTwo = "jupiter";
+//        String choiceThree = "saturn";
+//
+//        String correctAnswer = choiceThree;
+//
+//        // Write a print statement asking the question
+//        System.out.println(question);
+//
+//        // Write a print statement giving the answer choices
+//        System.out.println("Choose one of the following: " + choiceOne +
+//                ", " + choiceTwo + ", or " + choiceThree + ".");
+//
+//        // Have the user input an answer
+//        // Retrieve the user's input
+//        Scanner scanner = new Scanner(System.in);
+//        String input = scanner.next();
+//
+//        // If the user's input matches the correctAnswer...
+//        if (correctAnswer.equals(input.toLowerCase())) {
+//            // then the user is correct, and we want to print out a
+//            // congrats message to the user.
+//            System.out.println("Congrats! That’s the correct answer.");
+//        } else {
+//            // If the user's input does not match the correctAnswer…
+//            // then the user is incorrect, and we want to print out a
+//            // message saying that the user is incorrect as well
+//            // as what the correct choice was.
+//            System.out.println("You are incorrect. The correct answer is " +
+//                    correctAnswer);
+//        }
+        announceTeatime();
     }
 
 }
